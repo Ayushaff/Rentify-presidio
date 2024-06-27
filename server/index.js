1ae19066519e408f500b05851ae1b9a85404462d
Ayush Sharma <sharmayush11403@gmail.com>
2024-06-27 20:35:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const listingRoutes = require("./routes/listing.js")
 const bookingRoutes = require("./routes/booking.js")
 const userRoutes = require("./routes/user.js")
 
-const allowedOrigins = ['http://localhost:3000', 'https://rentify-presidio-hi.vercel.app','https://rentify-presidio-one.vercel.app','https://rentify-presidio-1.onrender.com'];
+const allowedOrigins = ['http://localhost:3000', 'https://rentify-presidio-hi.vercel.app','https://rentify-presidio-one.vercel.app','http://54.66.154.238'];
 
 // Configure CORS options
 const corsOptions = {
